49f0423d33954cb21f99320ca9d097664acf7a69
Những đứa trẻ tăng động nhìn xa trông rộng thấy những bóng hồng đang chờ mong ngóng chồng <ngodlamm@gmail.com>
2026-03-25 23:59:19 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -22,10 +22,10 @@ abstract class User{
         this.email = email;
         this.status = true;
         this.active = true;
-        this.question1 = question1;
-        this.question2 = question2;
-        this.answer1 = answer1;
-        this.answer2 = answer2;
+        this.question1 = question1.toLowerCase();
+        this.question2 = question2.toLowerCase();
+        this.answer1 = answer1.toLowerCase();
+        this.answer2 = answer2.toLowerCase();
         this.lastTimeChangePass = Instant.now();
     }
     public String getID(){
@@ -49,8 +49,12 @@ abstract class User{
     public boolean getActive(){
         return active;
     }
-    public void setUserName(String username){
-        this.username = username;
+    public boolean setUserName(String username){
+        if (username != null && !username.isEmpty()){
+            this.username = username;
+            return true;
+        }
+        return false;
     }
     public boolean setPassWord(String oldPassWord, String password){
         if (oldPassWord.equals(this.password)){
@@ -58,28 +62,20 @@ abstract class User{
             lastTimeChangePass = Instant.now();
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
-    public void setEmail(String email){
-        this.email = email;
+    public boolean setEmail(String email){
+        if (email != null && !email.isEmpty()){
+            this.email = email;
+            return true;
+        }
+        return false;
     }
     public void setStatus(){
-        if (status == true){
-            status = false;
-        }
-        else{
-            status = true;
-        }
+        status = !status;
     }
     public void setActive(){
-        if (active == true){
-            active = false;
-        }
-        else{
-            active = true;
-        }
+        active = !active;
     }
     public Instant getLastTimeChangePass(){
         return lastTimeChangePass;
@@ -90,56 +86,133 @@ abstract class User{
     public String getQuestion2(){
         return question2;
     }
-    public void setQuestion1(String question1){
-        this.question1 = question1;
-    }
-    public void setQuestion2(String question2){
-        this.question2 = question2;
-    }
-    public boolean verifyer(String answer1, String answer2){
-        if (this.answer1.equals(answer1) && this.answer2.equals(answer2)){
+    public boolean setQuestion1(String question1){
+        if (question1 != null && !question1.isEmpty()){
+            this.question1 = question1.toLowerCase();
             return true;
         }
         return false;
     }
-    public void forgotPassWord(String password){
-        this.password = password;
-        lastTimeChangePass = Instant.now();
+    public boolean setQuestion2(String question2){
+        if (question2 != null && !question2.isEmpty()){
+            this.question2 = question2.toLowerCase();
+            return true;
+        }
+        return false;
+    }
+    public boolean setAnswer1(String answer1){
+        if (answer1 != null && !answer1.isEmpty()){
+            this.answer1 = answer1.toLowerCase();
+            return true;
+        }
+        return false;
+    }
+    public boolean setAnswer2(String answer2){
+        if (answer2 != null && !answer2.isEmpty()){
+            this.answer2 = answer2.toLowerCase();
+            return true;
+        }
+        return false;
+    }
+    public boolean verifyer(String answer1, String answer2){
+        if (answer1 != null && !answer1.isEmpty() && answer2 != null && !answer2.isEmpty() && this.answer1.equals(answer1.toLowerCase()) && this.answer2.equals(answer2.toLowerCase())){
+            return true;
+        }
+        return false;
+    }
+    public boolean forgotPassWord(String password, String answer1, String answer2){
+        if (verifyer(answer1, answer2) && password != null && !password.isEmpty()){
+            this.password = password;
+            lastTimeChangePass = Instant.now();
+            return true;
+        }
+        return false;
     }
 }
 
 class UserService{
     private Map<String, User> userDB = new HashMap<>();
-    public boolean register(String id, String username, String password, String role, String email) {
+    public boolean register(String username, String password, String role, String email) {
+        if (username == null || password == null || email == null){
+            return false;
+        }
+        username = username.trim();
+        password = password.trim();
+        email = email.trim();
+        if (!Validator.validUserName(username)) {
+            return false;
+        }
+        if (!Validator.validPassWord(password)) {
+            return false;
+        }
+        if (!Validator.validEmail(email)) {
+            return false;
+        }
         if (userDB.containsKey(username)) {
             return false;
         }
         if (role.equals("BIDDER")) {
-            User user = new Bidder(id, username, password, role, email);
+            User user = new Bidder(username, password, role, email);
             userDB.put(username, user);
         } 
         else if (role.equals("SELLER")) {
-            User user = new Seller(id, username, password, role, email);
+            User user = new Seller(username, password, role, email);
             userDB.put(username, user);
         } 
         else if (role.equals("ADMIN")) {
-            User user = new Admin(id, username, password, role, email);
+            User user = new Admin(username, password, role, email);
             userDB.put(username, user);
         }
         return true;
     }
     public User login(String username, String password){
-        if (!userDB.containsKey(username)) {
-            return null;
+        if (username == null || password == null){
+            return false;
         }
+        username = username.trim();
+        password = password.trim();
         User user = userDB.get(username);
-        if (!user.getPassWord().equals(password)) {
-            return null;
-        }
-        if (user.getStatus() == false) {
+        if (user == null || !user.getPassword().equals(password) || !user.isActive()){
             return null;
         }
         return user;
+    }
+}
+
+class Validator{
+    public static boolean validUserName(String username) {
+        if (username == null){
+            return false;
+        }
+        username = username.trim();
+        if (username.isEmpty() || username.matches(".*\\s.*") || !username.matches("[a-zA-Z0-9_]+") || username.length() < 3 || username.length() > 15){
+            return false;
+        }
+        return true;
+    }
+    public static boolean validPassWord(String password){
+        if (password == null){
+            return false;
+        }
+        password = password.trim();
+        if (password.isEmpty() || password.matches(".*\\s.*") || password.length() < 6){
+            return false;
+        }
+        boolean hasLower = password.matches(".*[a-z].*");
+        boolean hasUpper = password.matches(".*[A-Z].*");
+        boolean hasDigit = password.matches(".*\\d.*");
+        boolean hasSpecial = password.matches(".*[^a-zA-Z0-9].*");
+        return hasDigit && hasLower && hasUpper && hasSpecial;
+    }
+    public static boolean validEmail(String email){
+        if (email == null){
+            return false;
+        }
+        email = email.trim();
+        if (email.isEmpty() || email.matches(".*\\s.*")){
+            return false;
+        }
+        return email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
     }
 }
 
