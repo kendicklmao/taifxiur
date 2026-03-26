9266be1c43ee0bfb3991db57d3426f6fa3392394
skillseedorg <email1.allinone@gmail.com>
2026-03-26 10:10:17 +0700

    Add getter and setter to Item

--- Main/src/Item.java
+++ Main/src/Item.java
@@ -15,4 +15,60 @@ abstract class Item{
         this.sellerName = sellerName;
         this.legitCheck = legitCheck;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public boolean isLegitCheck() {
+        return legitCheck;
+    }
+
+    public void setLegitCheck(boolean legitCheck) {
+        this.legitCheck = legitCheck;
+    }
 }
