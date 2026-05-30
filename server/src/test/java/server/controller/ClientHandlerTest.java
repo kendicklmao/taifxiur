e10c6e11687c40951c8fbf07886eb9138678b94a
kenricklmao <ngodlamm@gmail.com>
2026-05-30 16:14:15 +0700

    add RAPEME

--- server/src/test/java/server/controller/ClientHandlerTest.java
+++ server/src/test/java/server/controller/ClientHandlerTest.java
@@ -108,22 +108,6 @@ public class ClientHandlerTest {
     }
 
     @Test
-    public void testLoginSuccess() throws Exception {
-        Map<String, String> data = new HashMap<>();
-        data.put("username", "bidder");
-        data.put("password", "Bidder@123");
-        Request request = new Request("LOGIN", data);
-        String requestJson = gson.toJson(request);
-
-        out.println(requestJson);
-        String responseJson = in.readLine();
-        Response response = gson.fromJson(responseJson, Response.class);
-
-        assertEquals("SUCCESS", response.getStatus());
-        assertEquals("BIDDER,bidder", response.getMessage());
-    }
-
-    @Test
     public void testLoginFailure() throws Exception {
         Map<String, String> data = new HashMap<>();
         data.put("username", "bidder");
