25306230cccede3d70b583fef6d6ff8d17ed3752
ngo dailam <ngodlamm@gmail.com>
2026-06-02 10:29:16 +0700

    sủa test

--- server/src/test/java/server/service/UserServiceTest.java
+++ server/src/test/java/server/service/UserServiceTest.java
@@ -84,6 +84,17 @@ public class UserServiceTest {
 
     @Test
     public void testLoginUser() {
+        try (Connection conn = DatabaseConfig.getDataSource().getConnection();
+             PreparedStatement pstmt =
+                     conn.prepareStatement(
+                             "UPDATE users SET is_online = FALSE WHERE username = ?")) {
+
+            pstmt.setString(1, "lamdaingo");
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.err.println("Error updating online status: " + e.getMessage());
+        }
         assertNotNull(userService.login("lamdaingo", "Admin@123"));
         try (Connection conn = DatabaseConfig.getDataSource().getConnection();
              PreparedStatement pstmt =
